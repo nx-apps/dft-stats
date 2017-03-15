cb9aad1416e8290658efd05eb4f2ab90407e7c25
somchit chanudom <somchit.c@nexts-corp.com>
2017-03-15 16:38:37 +0700

    update

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -73,3 +73,50 @@ exports.h01 = function (req, res) {
             res.ireport("hamonize/report1.jasper", req.query.export || "pdf", data, param);
         });
 }
+exports.li01 = function (req, res) {
+    var r = req.r;
+    var s, e;
+    if (typeof req.query.sdate !== "undefined") {
+        s = new Date(req.query.sdate).getTime() // => 2016/01/01
+    } else {
+        s = new Date(today.replace("-", "/")).getTime()
+        //r.table('ec_head').orderBy({ index: 'approve_date' }).limit(1)(0).getField('approve_date')
+    }
+    if (typeof req.query.edate !== "undefined") {
+        e = new Date(req.query.edate).getTime() // => 2016/01/01
+    } else {
+        e = new Date(tomorrow.replace("-", "/")).getTime()
+        //r.table('ec_head').orderBy({ index: r.desc('approve_date') }).limit(1)(0).getField('approve_date')
+    }
+    r.expr({
+        sdate: s,
+        edate: e
+    })
+        .merge(function (hm) {
+            return r.table('ec_head').between(hm('sdate'), hm('edate'), { index: 'approve_date' })
+                .pluck('id', 'reference_code2', 'company_name', 'approve_date', 'expire_date')
+                .coerceTo('array')
+                .merge(function (dm) {
+                    return r.table('ec_data').getAll(dm('id'), { index: 'invh_run_auto' }).coerceTo('array')
+                        .pluck('hamonize_code', 'fob_amt_baht', 'net_weight')
+                        .merge(function (hm2) {
+                            return dm
+                        })
+
+                })
+        })
+        .reduce(function (l, r) {
+            return l.add(r)
+        })
+        .eqJoin('hamonize_code', r.table('hamonize_type')).pluck('left', { right: 'hamonize_th' }).zip()
+        .orderBy('reference_code2')
+        .run()
+        .then(function (data) {
+            for (var i in data) {
+                data[i].approve_date = new Date(data[i].approve_date).toLocaleString()
+                data[i].expire_date = new Date(data[i].expire_date).toLocaleString()
+            }
+            // res.json(data)
+            res.ireport("license/report1.jasper", req.query.export || "pdf", data);
+        });
+}
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
     var controller = require('../controllers/report.controller')
     app.get('/h01', controller.h01)
+    app.get('/li01', controller.li01)
 }
