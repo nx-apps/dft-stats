a0f78eeb03d22acf6cd771841a9f9d238c95096b
tonsai-j <tonsai.j@nexts-corp.com>
2017-03-15 20:14:33 +0700

    update componry

--- public/src/redux-store/reducer/company.js
+++ public/src/redux-store/reducer/company.js
@@ -13,6 +13,8 @@ export function companyReducer(state = initialState,action){
     switch (action.type) {
         case 'COMPANY_CODE_GET':
           return Object.assign({},state,{companyCodeList:action.payload});
+        case 'COMPANY_CODE_SEARCH_R1':
+          return Object.assign({},state,{list:action.payload});       
         default:
           return state
     }
@@ -23,27 +25,45 @@ export function companyAction(store){
     return [commonAction(),{
       COMPANY_CODE_GET(data){
           console.log(1);
-        // this.fire('toast',{status:'load',text:'กำลังบันทึกข้อมูล...'})
-        // axios.get('/company/')
-        // .then( (response)=>{
-        //     console.log(response);
-        //     store.dispatch({type:'COMPANY_CODE_GET',payload:response.data})
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        // });
+        this.fire('toast',{status:'load',text:'กำลังบันทึกข้อมูล...'})
+        axios.get('/company/')
+        .then( (response)=>{
+            console.log(response);
+            store.dispatch({type:'COMPANY_CODE_GET',payload:response.data})
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
       },
-      COMPANY_CODE_SEARCH(data){
+      COMPANY_CODE_SEARCH_R1(data=''){
         this.fire('toast',{status:'load',text:'กำลังค้นหาข้อมูล...'})
         console.log(data);
-        // axios.get('/company/')
-        // .then( (response)=>{
-        //     console.log(response);
-        //     store.dispatch({type:'company_CODE_GET',payload:response.data})
-        // })
-        // .catch(function (error) {
-        //     console.log(error);
-        // });
+        axios.get('/company/re01?'+data)
+        .then( (response)=>{
+            console.log(response);
+            this.fire('toast',{status:'success',text:'ค้นหาสำเร็จ',callback(){
+              store.dispatch({type:'COMPANY_CODE_SEARCH_R1',payload:response.data})
+            }});
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+      },
+      COMPANY_CODE_SEARCH_R2(data=''){
+        this.fire('toast',{status:'load',text:'กำลังค้นหาข้อมูล...'})
+        // console.log(data);
+        axios.get('/company/re02?'+data)
+        .then( (response)=>{
+            console.log(response);
+            this.fire('toast',{status:'success',text:'ค้นหาสำเร็จ',callback(){
+              store.dispatch({type:'COMPANY_CODE_SEARCH_R1',payload:response.data})
+            }});
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
       },
    }]
 };
