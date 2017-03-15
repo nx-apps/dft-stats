cad53abea79c7879e21cbec1d8ec6d452ae5d758
tonsai-j <tonsai.j@nexts-corp.com>
2017-03-15 20:02:06 +0700

    update setDate

--- public/src/redux-store/reducer/hamonize.js
+++ public/src/redux-store/reducer/hamonize.js
@@ -4,6 +4,7 @@ import {commonAction} from '../config'
 const initialState = {
     select:{},
     list:[],
+    date:{sdate:'',edate:''},
     hamonizeCodeList:[]
     // listFiles:[]
 }
@@ -11,6 +12,8 @@ const initialState = {
 export function hamonizeReducer(state = initialState,action){
 
     switch (action.type) {
+        case 'HAMONIZE_SET_DATE':
+          return Object.assign({},state,{date:action.payload});
         case 'HAMONIZE_CODE_GET':
           return Object.assign({},state,{hamonizeCodeList:action.payload});
         case 'HAMONIZE_CODE_SEARCH_R1' :
@@ -23,6 +26,13 @@ export function hamonizeReducer(state = initialState,action){
 
 export function hamonizeAction(store){
     return [commonAction(),{
+      HAMONIZE_SET_DATE(){
+          let date = new Object()
+          let today = new Date()
+          date.sdate = today.toISOString().split('T')[0]
+          date.edate = new Date(today.setDate(today.getDate()+1)).toISOString().split('T')[0]
+          store.dispatch({type:'HAMONIZE_SET_DATE',payload:date})
+      },
       HAMONIZE_CODE_GET(data){
         // this.fire('toast',{status:'load',text:'กำลังบันทึกข้อมูล...'})
         axios.get('/hamonize/')
@@ -36,8 +46,23 @@ export function hamonizeAction(store){
       },
       HAMONIZE_CODE_SEARCH_R1(data=''){
         this.fire('toast',{status:'load',text:'กำลังค้นหาข้อมูล...'})
-        // console.log(data);
+        console.log(data);
         axios.get('/hamonize/re01?'+data)
+        .then( (response)=>{
+            console.log(response);
+            this.fire('toast',{status:'success',text:'ค้นหาสำเร็จ',callback(){
+              store.dispatch({type:'HAMONIZE_CODE_SEARCH_R1',payload:response.data})
+            }});
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+      },
+      HAMONIZE_CODE_SEARCH_R2(data=''){
+        this.fire('toast',{status:'load',text:'กำลังค้นหาข้อมูล...'})
+        // console.log(data);
+        axios.get('/hamonize/re02?'+data)
         .then( (response)=>{
             console.log(response);
             this.fire('toast',{status:'success',text:'ค้นหาสำเร็จ',callback(){
