77df0f0cc057024a9ff23856a477be6de60f6c70
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-03-15 14:02:20 +0700

    fix sdate edate

--- app/controllers/hamonize.controller.js
+++ app/controllers/hamonize.controller.js
@@ -1,3 +1,20 @@
+var today = new Date();
+var dd = today.getDate();
+var dt = today.getDate() + 1;
+var mm = today.getMonth() + 1; //January is 0!
+
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+if (dt < 10) {
+    dt = '0' + dt;
+}
+if (mm < 10) {
+    mm = '0' + mm;
+}
+var today = '2016' + '-' + mm + '-' + dd;
+var tomorrow = '2016' + '-' + mm + '-' + dt;
 exports.list = function (req, res) {
     var r = req.r;
     r.table('hamonize_type')
@@ -59,9 +76,15 @@ exports.filter = function (req, res) {
 }
 exports.sp01 = function (req, res) {
     var j = req.jdbc;
-    j.query("mssql", `exec sp_hamonize01 @sdate=?,@edate=?`, [req.query.sdate, req.query.edate],
+    var s = today, e = tomorrow;
+    if (typeof req.query.sdate !== "undefined") {
+        s = req.query.sdate
+    }
+    if (typeof req.query.edate !== "undefined") {
+        e = req.query.edate
+    }
+    j.query("mssql", `exec sp_hamonize01 @sdate=?,@edate=?`, [s, e],
         function (err, data) {
             res.send(data)
         })
-
 }
--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -1,7 +1,30 @@
+var today = new Date();
+var dd = today.getDate();
+var dt = today.getDate() + 1;
+var mm = today.getMonth() + 1; //January is 0!
+
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+if (dt < 10) {
+    dt = '0' + dt;
+}
+if (mm < 10) {
+    mm = '0' + mm;
+}
+var today = '2016' + '-' + mm + '-' + dd;
+var tomorrow = '2016' + '-' + mm + '-' + dt;
 exports.hamonize01 = function (req, res) {
     var j = req.jdbc;
-
-    j.query("mssql", `exec sp_hamonize01 @sdate=?,@edate=?`, [req.query.sdate, req.query.edate],
+    var s = today, e = tomorrow;
+    if (typeof req.query.sdate !== "undefined") {
+        s = req.query.sdate
+    }
+    if (typeof req.query.edate !== "undefined") {
+        e = req.query.edate
+    }
+    j.query("mssql", `exec sp_hamonize01 @sdate=?,@edate=?`, [s, e],
         function (err, data) {
             res.send(data)
             //res.ireport(....)
