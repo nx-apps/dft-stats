4e7aee3d9292bd0ca3f4fe328b7b5272e75d0105
somchit chanudom <somchit.c@nexts-corp.com>
2017-07-18 12:01:26 +0700

    แก้ report ce01

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -110,7 +110,7 @@ exports.ce01 = function (req, res) {
         sdate: new Date(s).getFullYear() + "-" + (new Date(s).getMonth() + 1) + "-" + new Date(s).getDate(),
         edate: new Date(e).getFullYear() + "-" + (new Date(e).getMonth() + 1) + "-" + new Date(e).getDate(),
     };
-    j.query("mssql", `exec sp_qry_stats_cert @refCode=?, @startDate= ?, @endDate= ?`, [req.params.code || '', s, e],
+    j.query("mssql", `exec sp_qry_stats_cert @refCode=?, @startDate= ?, @endDate= ?`, [req.query.reference_code2 || '', s, e],
 
         // j.query("mssql", `select * from hamonize_type`, [],
         function (err, data) {
