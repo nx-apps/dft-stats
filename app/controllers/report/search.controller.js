2e1f08bf1fe273765dcb5637fdc18218c3979e42
somchit chanudom <somchit.c@nexts-corp.com>
2017-07-31 11:56:44 +0700

    update report2

--- app/controllers/report/search.controller.js
+++ app/controllers/report/search.controller.js
@@ -2,8 +2,6 @@ exports.hamonize = function (req, res) {
     var val = req.query;
     var date_start = req.query.date_start;
     var date_end = req.query.date_end;
-   
-
     if (req.method == 'POST') {
         val = req.body;
     }
@@ -11,8 +9,8 @@ exports.hamonize = function (req, res) {
         [val.year, val.hmcodes, val.date_start, val.date_end],
         function (err, data) {
             data = JSON.parse(data);
-            // res.json(data);
-            res.ireport("search/report1.jasper", req.query.export || "pdf", data, { date_start, date_end});
+            res.json(data);
+            res.ireport("search/hamonize.jasper", req.query.export || "pdf", data, { date_start, date_end });
 
         })
 }
@@ -20,17 +18,17 @@ exports.company = function (req, res) {
     var val = req.query;
     var date_start = req.query.date_start;
     var date_end = req.query.date_end;
-   
+
 
     if (req.method == 'POST') {
         val = req.body;
     }
     req.jdbc.query('mssql', 'exec sp_stats_query_company @taxNo=?,@dateStart=?,@dateEnd=?',
-        [val.taxNo,  val.date_start, val.date_end],
+        [val.taxNo, val.date_start, val.date_end],
         function (err, data) {
             data = JSON.parse(data);
-            res.json(data);
-            res.ireport("search/report2.jasper", req.query.export || "pdf", data, { date_start, date_end});
+            // res.json(data);
+            res.ireport("search/company.jasper", req.query.export || "pdf", data, { date_start, date_end });
 
         })
 }
