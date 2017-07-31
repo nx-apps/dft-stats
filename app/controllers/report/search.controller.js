33c4c9ceb4463623c38bffc2ff8fadf69edae38d
somchit chanudom <somchit.c@nexts-corp.com>
2017-07-31 10:28:18 +0700

    report2

--- app/controllers/report/search.controller.js
+++ app/controllers/report/search.controller.js
@@ -16,3 +16,21 @@ exports.hamonize = function (req, res) {
 
         })
 }
+exports.company = function (req, res) {
+    var val = req.query;
+    var date_start = req.query.date_start;
+    var date_end = req.query.date_end;
+   
+
+    if (req.method == 'POST') {
+        val = req.body;
+    }
+    req.jdbc.query('mssql', 'exec sp_stats_query_company @taxNo=?,@dateStart=?,@dateEnd=?',
+        [val.taxNo,  val.date_start, val.date_end],
+        function (err, data) {
+            data = JSON.parse(data);
+            res.json(data);
+            res.ireport("search/report2.jasper", req.query.export || "pdf", data, { date_start, date_end});
+
+        })
+}
--- app/routes/report/search.routes.js
+++ app/routes/report/search.routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var controller = require('../../controllers/report/search.controller')
     app.route('/hamonize').get(controller.hamonize).post(controller.hamonize);
+    app.route('/company').get(controller.company).post(controller.company);
 
 }
