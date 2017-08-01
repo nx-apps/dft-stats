d8e4b6e16a34d8c04695ad3b94026be80107eee8
somchit chanudom <somchit.c@nexts-corp.com>
2017-08-01 18:04:39 +0700

    update report1

--- app/controllers/report/search.controller.js
+++ app/controllers/report/search.controller.js
@@ -1,34 +1,50 @@
 exports.hamonize = function (req, res) {
     var val = req.query;
-    var date_start = req.query.date_start;
-    var date_end = req.query.date_end;
+    var dateStart = req.query.dateStart;
+    var dateEnd = req.query.dateEnd;
     if (req.method == 'POST') {
         val = req.body;
     }
     req.jdbc.query('mssql', 'exec sp_stats_query_hamonize @hmYear=?,@hmCode=?,@dateStart=?,@dateEnd=?',
-        [val.year, val.hmcodes, val.date_start, val.date_end],
+        [val.year, val.hmcodes, val.dateStart, val.dateEnd],
         function (err, data) {
             data = JSON.parse(data);
-            res.json(data);
-            res.ireport("search/hamonize.jasper", req.query.export || "pdf", data, { date_start, date_end });
+            // res.json(data);
+            res.ireport("search/hamonize.jasper", req.query.export || "pdf", data, { dateStart, dateEnd });
 
         })
 }
 exports.company = function (req, res) {
     var val = req.query;
-    var date_start = req.query.date_start;
-    var date_end = req.query.date_end;
+    var dateStart = req.query.dateStart;
+    var dateEnd = req.query.dateEnd;
 
 
     if (req.method == 'POST') {
         val = req.body;
     }
     req.jdbc.query('mssql', 'exec sp_stats_query_company @taxNo=?,@dateStart=?,@dateEnd=?',
-        [val.taxNo, val.date_start, val.date_end],
+        [val.taxNo, val.dateStart, val.dateEnd],
         function (err, data) {
             data = JSON.parse(data);
             // res.json(data);
-            res.ireport("search/company.jasper", req.query.export || "pdf", data, { date_start, date_end });
+            res.ireport("search/company.jasper", req.query.export || "pdf", data, { dateStart, dateEnd });
+
+        })
+}
+exports.referenceCode = function (req, res) {
+    var val = req.query;
+   
+
+    if (req.method == 'POST') {
+        val = req.body;
+    }
+    req.jdbc.query('mssql', 'exec sp_stats_rpt_reference_code @refCode=?',
+        [val.refCode],
+        function (err, data) {
+            data = JSON.parse(data);
+            res.json(data);
+            // res.ireport("search/report1.jasper", req.query.export || "pdf", data);
 
         })
 }
--- app/routes/report/search.routes.js
+++ app/routes/report/search.routes.js
@@ -2,5 +2,6 @@ module.exports = function (app) {
     var controller = require('../../controllers/report/search.controller')
     app.route('/hamonize').get(controller.hamonize).post(controller.hamonize);
     app.route('/company').get(controller.company).post(controller.company);
+    app.route('/referenceCode').get(controller.referenceCode).post(controller.referenceCode);
 
 }
