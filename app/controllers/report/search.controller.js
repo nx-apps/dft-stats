87448cb92299da0f25cd1ce234b9228549f78e4f
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-08-02 18:08:04 +0700

    ec

--- app/controllers/report/search.controller.js
+++ app/controllers/report/search.controller.js
@@ -30,7 +30,7 @@ exports.company = function (req, res) {
 
         })
 }
-exports.referenceCode = function (req, res) {
+exports.expt = function (req, res) {
     var val = req.query;
 
 
@@ -69,7 +69,59 @@ exports.referenceCode = function (req, res) {
             var main = data[0];
             main.sub = sub;
             // res.json(main);
-            res.ireport("search/report1.jasper", req.query.export || "pdf", [main], {});
+            res.ireport("search/export.jasper", req.query.export || "pdf", [main], {});
 
         })
+}
+exports.impt = function (req, res) {
+    // res.ireport("search/import.jasper", req.query.export || "pdf", [{}], {});
+    var val = req.query;
+    if (req.method == 'POST') val = req.body;
+    req.jdbc.query('mssql', 'exec sp_stats_rpt_reference_code @refCode=?',
+        [val.refCode],
+        function (err, data) {
+            data = JSON.parse(data);
+            var len = data.length;
+            var sub = [];
+            for (var i = 0; i < 2; i++) {
+                if (i < len) {
+                    sub.push({
+                        product_description: data[i].product_description,
+                        tariff_code: data[i].tariff_code,
+                        net_weight: data[i].net_weight,
+                        net_weight_unit: data[i].net_weight_unit,
+                        fob_amt_perunit: data[i].fob_amt_perunit,
+                        currency_code: data[i].currency_code,
+                        fob_amt_baht: data[i].fob_amt_baht
+                    });
+                } else {
+                    sub.push({
+                        product_description: '*********************************************************',
+                        tariff_code: '',
+                        net_weight: 0,
+                        net_weight_unit: '',
+                        fob_amt_perunit: 0,
+                        currency_code: '',
+                        fob_amt_baht: 0
+                    });
+                }
+            }
+            var main = data[0];
+            main.sub = sub;
+            // res.json(main);
+            res.ireport("search/import.jasper", req.query.export || "pdf", [main], {});
+
+        })
+}
+exports.ec = function (req, res) {
+    // res.ireport("search/import.jasper", req.query.export || "pdf", [{}], {});
+    var val = req.query;
+    if (req.method == 'POST') val = req.body;
+    req.jdbc.query('mssql', 'exec sp_stats_rpt_reference_code @refCode=?',
+        [val.refCode],
+        function (err, data) {
+            data = JSON.parse(data);
+            // res.json(main);
+            res.ireport("search/ec.jasper", req.query.export || "pdf", data, {});
+        });
 }
--- app/routes/report/search.routes.js
+++ app/routes/report/search.routes.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
     var controller = require('../../controllers/report/search.controller')
     app.route('/hamonize').get(controller.hamonize).post(controller.hamonize);
     app.route('/company').get(controller.company).post(controller.company);
-    app.route('/referenceCode').get(controller.referenceCode).post(controller.referenceCode);
-
+    app.route('/expt').get(controller.expt).post(controller.expt);
+    app.route('/impt').get(controller.impt).post(controller.impt);
+    app.route('/ec').get(controller.ec).post(controller.ec);
 }
