d48d68ba47d4911db9e624c4cb1b1bf1f155e1ea
somchit chanudom <somchit.c@nexts-corp.com>
2017-08-04 11:53:01 +0700

    แก้ report

--- app/controllers/report.controller.js
+++ app/controllers/report.controller.js
@@ -126,7 +126,7 @@ exports.dailyCompany = function (req, res) {
     req.jdbc.query("mssql", "exec sp_stats_rpt_daily_company @approveDate=?", [req.query.date], function (err, data) {
         // res.send(data);
         req.r.json(data).run().then(function (d2) {
-            res.ireport("daily/rpt_invioce_company.jasper", req.query.export || "pdf", d2, { approveDate: req.query.date });
+            res.ireport("daily/rpt_daily_company.jasper", req.query.export || "pdf", d2, { approveDate: req.query.date,FILE_TYPE:req.query.export });
         })
     })
 }
@@ -142,12 +142,13 @@ exports.dailyCountry = function (req, res) {
     var param = {
         sdate: new Date(s).getFullYear() + "-" + (new Date(s).getMonth() + 1) + "-" + new Date(s).getDate(),
         edate: new Date(e).getFullYear() + "-" + (new Date(e).getMonth() + 1) + "-" + new Date(e).getDate(),
+        FILE_TYPE:req.query.export
     };
     j.query("mssql", "exec sp_stats_rpt_daily_country @startDate=?, @endDate=?", [s, e],
         function (err, data) {
             // res.send(data);
             req.r.json(data).run().then(function (d2) {
-                res.ireport("daily/rpt_invioce_country.jasper", req.query.export || "pdf", d2, param);
+                res.ireport("daily/rpt_daily_country.jasper", req.query.export || "pdf", d2, param);
             })
         })
 }
@@ -156,12 +157,15 @@ exports.dailyPricerice = function (req, res) {
     req.jdbc.query("mssql", "exec sp_stats_rpt_daily_pricerice @approveDate=?", [req.query.date], function (err, data) {
         // res.send(data);
         req.r.json(data).run().then(function (d2) {
-            res.ireport("daily/rpt_invioce_pricerice.jasper", req.query.export || "pdf", d2, { approveDate: req.query.date });
+            res.ireport("daily/rpt_daily_pricerice.jasper", req.query.export || "pdf", d2, { 
+                approveDate: req.query.date,
+                FILE_TYPE:req.query.export
+            });
         })
     })
 }
 exports.dailyExportrice = function (req, res) {
-
+    var refData = (req.query.refData == 'edi' ? 'ใบอนุญาตส่งออกข้าว' : 'กรมศุลกากร');
     req.jdbc.query("mssql", "exec sp_stats_rpt_daily_exportrice @currentDate=?, @amountTime=?",
         [req.query.currentDate, req.query.amountTime],
         function (err, data) {
@@ -171,7 +175,8 @@ exports.dailyExportrice = function (req, res) {
             // .run().then(function (d2) {
             res.ireport("daily/rpt_daily_exportrice.jasper", req.query.export || "pdf", data, {
                 OUTPUT_NAME: 'สรุปสถานการณ์ส่งออกข้าว' + req.query.currentDate.replace('-', '_'),
-                CURRENT_DATE: req.query.currentDate
+                CURRENT_DATE: req.query.currentDate,
+                REF_DATA: refData
             });
             // })
         })
--- app/controllers/report/search.controller.js
+++ app/controllers/report/search.controller.js
@@ -1,14 +1,17 @@
 exports.hamonize = function (req, res) {
     var val = req.query;
-    // var dateStart = req.query.dateStart;
-    // var dateEnd = req.query.dateEnd;
+    var dateStart = req.query.dateStart;
+    var dateEnd = req.query.dateEnd;
     if (req.method == 'POST') val = req.body;
     req.jdbc.query('mssql', 'exec sp_stats_query_hamonize @hmYear=?,@hmCode=?,@dateStart=?,@dateEnd=?',
         [val.hmYear, val.hmCode, val.dateStart, val.dateEnd],
         function (err, data) {
             data = JSON.parse(data);
             // res.json(data);
-            res.ireport("search/hamonize.jasper", req.query.export || "pdf", data, val);
+            res.ireport("search/hamonize.jasper", req.query.export || "pdf", data, {
+                dateStart, dateEnd,
+                FILE_TYPE: req.query.export
+            });
 
         })
 }
@@ -26,7 +29,10 @@ exports.company = function (req, res) {
         function (err, data) {
             data = JSON.parse(data);
             // res.json(data);
-            res.ireport("search/company.jasper", req.query.export || "pdf", data, { dateStart, dateEnd });
+            res.ireport("search/company.jasper", req.query.export || "pdf", data, {
+                dateStart, dateEnd,
+                FILE_TYPE: req.query.export
+            });
 
         })
 }
