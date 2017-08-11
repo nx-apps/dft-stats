9618d29035c9d76d44627e9cf0fe0a0ba92ba846
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-08-11 17:31:54 +0700

    update price

--- app/controllers/price.controller.js
+++ app/controllers/price.controller.js
@@ -24,3 +24,26 @@ exports.today = function (req, res) {
             res.json(data)
         })
 }
+exports.update = function (req, res) {
+    r.expr(req.body).pluck(
+        'id',
+        'price_dit',
+        'price_fob',
+        'price_thai',
+        'price_paddy',
+        'price_usa',
+        'price_india',
+        'price_vietnam',
+        'price_pakistan'
+    ).forEach(function (fe) {
+        return r.table('price').get(fe('id'))
+            .update(
+            fe.merge({
+                price_date: r.ISO8601(fe('price_date')).inTimezone('+07')
+            })
+            )
+    })
+        .run().then(function (data) {
+            res.json(data)
+        })
+}
--- app/routes/price.routes.js
+++ app/routes/price.routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
     var controller = require('../controllers/price.controller')
-    app.get('/today', controller.today)
-
+    app.get('/today', controller.today);
+    app.put('/update',controller.update);
 }
