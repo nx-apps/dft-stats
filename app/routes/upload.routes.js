3363f2bde23f5e66066e219fc37300d2e6b0e086
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-08-15 18:13:02 +0700

    upload

--- app/routes/upload.routes.js
+++ app/routes/upload.routes.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
     var controller = require('../controllers/upload.controller')
+    app.get('/',controller.listUpload)
     app.post('/mc', controller.uploadMc)
 }
