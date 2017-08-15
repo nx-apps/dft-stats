88b8af14b6e86952b71282d3151896d383229cd8
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-08-15 17:21:28 +0700

    upload countroller

--- app/controllers/upload.controller.js
+++ app/controllers/upload.controller.js
@@ -8,20 +8,30 @@ exports.uploadMc = function (req, res) {
 
     form.parse(req, function (err, fields, files) {
         var prefile = files.file[0];
-        let fileName = prefile.headers['content-disposition'].split('"')
+        let time = new Date().toISOString().split('T')[0]
+        let fileName = prefile.headers['content-disposition'].split('"')[3]
+        let newFileName = time + '-' + fileName
         // console.log(fileName[3]);
         fs.readFile(prefile.path, function (err, data) {
             // console.log(data);
-            fs.writeFile('./public/files/' + fileName[3], data, ['utf8'], (err) => {
+            fs.writeFile('./public/files/' + newFileName, data, ['utf8'], (err) => {
                 if (err) {
                     return console.log(err);
                 }
                 else {
-                    // console.log("The file was saved!");
-                    res.status(200).json('FIN');
-                    // res.writeHead(200, { 'content-type': 'text/plain' });
-                    // res.write('received upload:\n\n');
-                    // res.end(util.inspect({ fields: fields, files: files }));
+                    let data = {
+                        file_name: fileName,
+                        file_path: newFileName,
+                        data_uploaded: r.now().inTimezone('+07'),
+                    }
+                    r.db('stats').table('upload')
+                        .insert(data)
+                        .run()
+                        .then(function (result) {
+                            res.json(result);
+                        }).catch(function (err) {
+                            res.json(err);
+                        })
                 }
             })
         })
