f551eef5f75cbd09b61004770af007716fe47dbd
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-08-28 10:20:14 +0700

    excel price config

--- app/controllers/excel.controller.js
+++ app/controllers/excel.controller.js
@@ -1,24 +1,5 @@
 var XLSX = require('xlsx');
 exports.read = function (req, res) {
-    var indexArr = {
-        ref_code: 'เลขที่อ้างอิง',
-        std_code: 'เลขที่ มส.24',
-        std_date: 'วันที่ออก มส.24',
-        company_name: 'ผู้ส่งออก',
-        product_name: 'สินค้า',
-        type_name: 'ประเภท/ชนิด/ชั้น',
-        std_no: 'รายการที่',
-        quantity: 'ปริมาณ',
-        unit_desc: 'หน่วย',
-        weight_km: 'น้ำหนักเฉพาะ (กก.)',
-        weight_ton: 'น้ำหนัก (เมตริกตัน)',
-        value_b: 'มูลค่า (บาท)',
-        rate_bank: 'อัตราแลกเปลี่ยน',
-        dest_city: 'เมืองตราส่ง',
-        dest_country: 'ประเทศ',
-        buyer_name: 'ชื่อผู้ซื้อต่างประเทศ',
-        buyer_country: 'ประเทศผู้ซื้อ'
-    };
     r.table('upload').get(req.query.id)
         .run()
         .then(function (fileUpload) {
@@ -32,50 +13,6 @@ exports.read = function (req, res) {
                 });
             }
             res.json(data);
-            // // res.json(file);
-            // var data = {};
-            // var temp = { db: "", col: [], maxCol: "" };
-            // var keyIndex = 1; //num row has field_key
-            // var row = {};
-            // // // res.json(file);
-            // for (var sheet in file) {
-            //     for (var key in file[sheet]) {
-            //         if (key !== '!ref' && key !== '!margins' && key !== '!autofilter' && key !== '!range') {
-            //             // console.log('data ',c++);
-            //             if (str2NumOnly(key) == keyIndex) {
-            //                 // temp.col[str2CharOnly(key)] = getKeyByValue(indexArr, file[sheet][key].v);
-            //                 temp.col[str2CharOnly(key)] = file[sheet][key].v;
-            //                 temp.maxCol = str2CharOnly(key);
-            //             } else {
-            //                 // if(temp.col[str2CharOnly(key)]=='วันที่ออก มส.24'){
-            //                 //     console.log(file[sheet][key]);
-            //                 // }
-            //                 if (temp.col[str2CharOnly(key)].indexOf("วันที่") > -1) {
-            //                     row[temp.col[str2CharOnly(key)]] = file[sheet][key].w;
-            //                 } else {
-            //                     row[temp.col[str2CharOnly(key)]] = file[sheet][key].v;
-            //                 }
-            //                 if (str2CharOnly(key) == temp.maxCol) {
-            //                     data[temp.db].push(row);
-            //                     // data.push(row);
-            //                     row = {};
-            //                 }
-            //             }
-
-            //         } else {
-            //             temp.col = [];
-            //             temp.db = sheet;
-            //             if (!data.hasOwnProperty(sheet)) {
-            //                 data[sheet] = [];
-            //             }
-            //         }
-            //     }
-            // }
-            // var dataSheet = [];
-            // for (table in data) {
-            //     dataSheet.push({ sheet: table, data: data[table] });
-            // }
-            // res.json(dataSheet);
         })
 
 }
@@ -143,6 +80,17 @@ exports.convert = function (req, res) {
             res.json(data);
         })
 }
+ r.expr(dataSheet).forEach(function (row) {
+        return r.db('stats').tableList().contains(row('table'))
+            .do(function (tbExists) {
+                return r.branch(tbExists,
+                    r.db('stats').table(row('table')).delete(),
+                    r.db('stats').tableCreate(row('table'))
+                ).do(function (tbInsert) {
+                    return r.db('stats').table(row('table')).insert(row('data'))
+                })
+            })
+    })
 function str2NumOnly(string) { //input AB123  => output 123
     let t = [];
     for (let i = 0; i < string.length; i++) {
