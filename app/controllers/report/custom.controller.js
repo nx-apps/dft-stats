25e40462633878800643edc2cb734f0006e5d11d
lissara.k <lissara.k@nexts-corp.com>
2017-09-25 18:17:44 +0700

    report custom exportrice

--- app/controllers/report/custom.controller.js
+++ app/controllers/report/custom.controller.js
@@ -13,25 +13,26 @@ exports.exportrice = function (req, res) {
         var n = (i < 10 ? '0' + i : i);
         monthArr.push(n)
     }
-    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_exportrice @month=?, @year", [monthArr.join(','), req.query.year,],
+    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_exportrice @month=?, @year=?", [monthArr.join(','), req.query.year,],
         function (err, data) {
-            res.send(data);
+            // res.send(data);
             // console.log(req.query.year)
-            // req.r.json(data).run().then(function (d2) {
-            //     var param = {
-            //         date: req.query.date
-            //     };
-            //     param = rpt.keysToUpper(param);
-            //     param.startMonth = rpt.getMonthName(Number(req.query.startMonth));
-            //     param.endMonth = rpt.getMonthName(Number(req.query.endMonth));
-            //     param.current_date = new Date().toISOString().slice(0, 10);
-            //     // res.json(param)
-            //     // res.ireport("custom/rpt_custom_exportrice.jasper", req.query.export || "pdf", d2, {
-            //     //     approveDate: req.query.date,
-            //     //     FILE_TYPE: req.query.export,
-            //     //     OUTPUT_NAME: params.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวขาวของไทย'
-            //     // });
-            // })
+            req.r.json(data).run().then(function (d2) {
+                var param = {
+                    date: req.query.date
+                };
+                param.startMonth = rpt.getMonthName(Number(req.query.startMonth));
+                param.endMonth = rpt.getMonthName(Number(req.query.endMonth));
+                param.year = req.query.year;
+                param.current_date = new Date().toISOString().slice(0, 10);
+                param = rpt.keysToUpper(param);
+                // res.json(param)
+                res.ireport("custom/rpt_custom_exportrice.jasper", req.query.export || "pdf", d2, param)
+                //     approveDate: req.query.date,
+                //     FILE_TYPE: req.query.export,
+                //     OUTPUT_NAME: params.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวขาวของไทย'
+                // });
+            })
         })
 }
 
