690bfd4329b368aea0319117d7618a3753fddba3
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-28 21:02:30 +0700

    alias name

--- app/controllers/custom.controller.js
+++ app/controllers/custom.controller.js
@@ -1,13 +1,20 @@
 exports.zone = function (req, res) {
     var j = req.jdbc;
-    j.query("mssql", `SELECT zone_name from custom_country group by zone_name`, [],
+    j.query("mssql", `SELECT zone_name as zone from custom_country group by zone_name`, [],
         function (err, data) {
             res.send(data)
         })
 }
 exports.year = function (req, res) {
     var j = req.jdbc;
-    j.query("mssql", `SELECT model_year from custom group by model_year`, [],
+    j.query("mssql", `SELECT model_year as modelYear from custom group by model_year`, [],
+        function (err, data) {
+            res.send(data)
+        })
+}
+exports.month = function (req, res) {
+    var j = req.jdbc;
+    j.query("mssql", `SELECT model_month as modelMonth from custom where model_year = ? group by model_month`, [req.query.modelYear],
         function (err, data) {
             res.send(data)
         })
--- app/routes/custom.routes.js
+++ app/routes/custom.routes.js
@@ -1,5 +1,6 @@
-module.exports=function(app){
+module.exports = function (app) {
     var controller = require('../controllers/custom.controller');
-    app.get('/zone',controller.zone);
-    app.get('/year',controller.year);
+    app.get('/zone', controller.zone);
+    app.get('/year', controller.year);
+    app.get('/month', controller.month);
 }
