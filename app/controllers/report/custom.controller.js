e29a5dd6c12a4b73a72eb7617609e5dd4b31cec3
lissara.k <lissara.k@nexts-corp.com>
2017-09-28 17:17:58 +0700

    report custom

--- app/controllers/report/custom.controller.js
+++ app/controllers/report/custom.controller.js
@@ -6,20 +6,23 @@ exports.whiterice = function (req, res) {
         var n = (i < 10 ? '0' + i : i);
         monthArr.push(n)
     }
-    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_export_whiterice @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
+    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_whiterice @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
         function (err, data) {
             // res.send(data);
             // console.log(req.query.year)
             req.r.json(data).run().then(function (d2) {
                 var param = {
-                    date: req.query.date
+                    date: req.query.date,
+                    FILE_TYPE: req.query.export,
+                    startMonth : rpt.getMonthName(Number(req.query.startMonth)),
+                    endMonth : rpt.getMonthName(Number(req.query.endMonth)),
+                    year : req.query.year,
                 };
-                param.startMonth = rpt.getMonthName(Number(req.query.startMonth));
-                param.endMonth = rpt.getMonthName(Number(req.query.endMonth));
-                param.year = req.query.year;
-                param.current_date = new Date().toISOString().slice(0, 10);
                 param = rpt.keysToUpper(param);
-                // res.json(d2)
+                param.current_date = new Date().toISOString().slice(0, 10);
+                param.OUTPUT_NAME = param.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวขาวของไทย'
+                // console.log(param.OUTPUT_NAME)
+                // res.json(param)
                 res.ireport("custom/rpt_custom_export_whiterice.jasper", req.query.export || "pdf", d2, param)
                 //     approveDate: req.query.date,
                 //     FILE_TYPE: req.query.export,
@@ -35,23 +38,25 @@ exports.exportrice = function (req, res) {
         var n = (i < 10 ? '0' + i : i);
         monthArr.push(n)
     }
-    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_export_rice @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
+    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_rice @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
         function (err, data) {
             // res.send(data);
             req.r.json(data).run().then(function (d2) {
                 var param = {
-                    date: req.query.date
+                    date: req.query.date,
+                    FILE_TYPE: req.query.export,
                 };
                 param.startMonth = rpt.getMonthName(Number(req.query.startMonth));
                 param.endMonth = rpt.getMonthName(Number(req.query.endMonth));
                 param.year = req.query.year;
                 param.current_date = new Date().toISOString().slice(0, 10);
+                param.OUTPUT_NAME = param.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวของไทย'
                 param = rpt.keysToUpper(param);
                 // res.json(param)
                 res.ireport("custom/rpt_custom_export_rice.jasper", req.query.export || "pdf", d2, param)
                 //     approveDate: req.query.date,
                 //     FILE_TYPE: req.query.export,
-                //     OUTPUT_NAME: params.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวขาวของไทย'
+                //     OUTPUT_NAME: params.current_date.replace(/-/g, '') + '_ปริมาณและมูลค่าการส่งออกข้าวของไทย'
                 // });
             })
         })
@@ -63,17 +68,19 @@ exports.hommalirice = function (req, res) {
         var n = (i < 10 ? '0' + i : i);
         monthArr.push(n)
     }
-    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_export_thaihommalirice @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
+    req.jdbc.query("mssql", "exec sp_stats_rpt_custom_hommali @month=?, @year=?, @zone=?", [monthArr.join(','), req.query.year, req.query.zone],
         function (err, data) {
             // res.send(data);
             req.r.json(data).run().then(function (d2) {
                 var param = {
-                    date: req.query.date
+                    date: req.query.date,
+                    FILE_TYPE: req.query.export,
                 };
                 param.startMonth = rpt.getMonthName(Number(req.query.startMonth));
                 param.endMonth = rpt.getMonthName(Number(req.query.endMonth));
                 param.year = req.query.year;
                 param.current_date = new Date().toISOString().slice(0, 10);
+                param.OUTPUT_NAME = param.current_date.replace(/-/g, '') + '_ปริมาณมูลค่าการส่งออกข้าวหอมมะลิไทย'
                 param = rpt.keysToUpper(param);
                 // res.json(param)
                 res.ireport("custom/rpt_custom_export_hommalirice.jasper", req.query.export || "pdf", d2, param)
