b79d4ce08ac8668c9f3af864588473906076f40d
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-10-02 13:27:15 +0700

    custom typerice

--- app/controllers/custom.controller.js
+++ app/controllers/custom.controller.js
@@ -24,9 +24,34 @@ exports.month = function (req, res) {
             res.json(data)
         })
 }
+exports.typericeGroup = function (req, res) {
+    var j = req.jdbc;
+    j.query("mssql", `SELECT typerice_name,hscode as value,hm.hamonize_th as label
+    from custom_hscode ch
+    join hamonize hm on hm.hamonize_code=ch.hscode and hm.hamonize_year=2017
+    group by typerice_name,hscode,hm.hamonize_th
+    order by typerice_name,hscode`, [],
+        function (err, data) {
+            data = JSON.parse(data);
+            var arr = [];
+            var temp = '';
+            for (var i = 0; i < data.length; i++) {
+                if (temp != data[i].typerice_name) {
+                    arr.push({ group_name: data[i].typerice_name, group_item: [] })
+                    temp = data[i].typerice_name
+                }
+                arr.filter((f) => f.group_name == data[i].typerice_name)[0].group_item.push(data[i])
+            }
+            res.json(arr)
+        })
+}
 exports.typerice = function (req, res) {
     var j = req.jdbc;
-    j.query("mssql", `SELECT typerice_name as value,typerice_name as label from custom_hscode group by typerice_name`, [],
+    j.query("mssql", `SELECT hscode as value,hm.hamonize_th as label
+    from custom_hscode ch
+    join hamonize hm on hm.hamonize_code=ch.hscode and hm.hamonize_year=2017
+    group by hscode,hm.hamonize_th
+    order by hscode`, [],
         function (err, data) {
             res.send(data)
         })
--- app/routes/custom.routes.js
+++ app/routes/custom.routes.js
@@ -3,5 +3,6 @@ module.exports = function (app) {
     app.get('/zone', controller.zone);
     app.get('/year', controller.year);
     app.get('/month', controller.month);
+    app.get('/typerice/group', controller.typericeGroup);
     app.get('/typerice', controller.typerice);
 }
