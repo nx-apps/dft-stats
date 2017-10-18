0b2b12b3382666537031e1ac9a63b3695edeb4a3
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-10-18 15:58:06 +0700

    update for build

--- public/src/redux-store/index.js
+++ public/src/redux-store/index.js
@@ -5,8 +5,8 @@ import axios from './axios'
 import {commonSystemReducer,commonSystemAction} from './reducer/commonSystem'
 import {authReducer,authAction} from './reducer/auth'
 import {providerReducer,providerAction} from './reducer/provider'
-import {uploadReducer,uploadAction} from './reducer/upload'
 
+import {uploadReducer,uploadAction} from './reducer/upload'
 import {hamonizeReducer,hamonizeAction} from './reducer/hamonize'
 import {companyReducer,companyAction} from './reducer/company'
 import {licenseReducer,licenseAction} from './reducer/license'
@@ -41,20 +41,23 @@ const storeApp = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-window.ReduxBehavior = [PolymerRedux(storeApp),dispatchActionBehavior()];
-window.dispatchActionBehavior = dispatchActionBehavior();
-window.axios = axios;
-window.commonSystemAction = commonSystemAction(storeApp);
-window.authAction = authAction(storeApp);
-window.providerAction = providerAction(storeApp);
-window.uploadAction = uploadAction(storeApp);
+// window.ReduxBehavior = [PolymerRedux(storeApp),dispatchActionBehavior()];
+// window.dispatchActionBehavior = dispatchActionBehavior();
+window.preReduxBehavior = PolymerRedux(storeApp)
+window.preDispatchActionBehavior = dispatchActionBehavior()
+window.dispatchActionBehavior = dispatchActionBehavior()
 
-window.hamonizeAction = hamonizeAction(storeApp);
-window.companyAction = companyAction(storeApp);
-window.companyAction = companyAction(storeApp);
-window.countryAction = countryAction(storeApp);
-window.certAction = certAction(storeApp);
-window.pricehamonizeAction = pricehamonizeAction(storeApp);
-window.searchAction = searchAction(storeApp);
-window.searchCustomsAction = searchCustomsAction(storeApp);
-window.reportAction = reportAction(storeApp);
+window.axios = axios;
+window.precommonSystemAction = commonSystemAction(storeApp);
+window.preauthAction = authAction(storeApp);
+window.preproviderAction = providerAction(storeApp);
+window.preuploadAction = uploadAction(storeApp);
+
+window.prehamonizeAction = hamonizeAction(storeApp);
+window.precompanyAction = companyAction(storeApp);
+window.precountryAction = countryAction(storeApp);
+window.precertAction = certAction(storeApp);
+window.prepricehamonizeAction = pricehamonizeAction(storeApp);
+window.presearchAction = searchAction(storeApp);
+window.presearchCustomsAction = searchCustomsAction(storeApp);
+window.prereportAction = reportAction(storeApp);
